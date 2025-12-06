eb121af4a50adf22d80fa2e64783b98a5860abf2
mehdi el-hajjame <el-hajjame@outlook.com>
2025-12-06 23:43:56 +0100

    progress Bar done

--- script.js
+++ script.js
@@ -2,68 +2,91 @@
 
 
 const questions = [
-  {
-    question: 'Which of the following is used to declare a variable in JavaScript?',
-    answer: [
-      { text: 'var', correct: true },
-      { text: 'vbl', correct: false },
-      { text: 'letgo', correct: false },
-      { text: 'variable', correct: false },
-    ]
-  },
-  {
-    question: 'What does `===` operator check in JavaScript?',
-    answer: [
-      { text: 'Only value equality', correct: false },
-      { text: 'Only type equality', correct: false },
-      { text: 'Value and type equality', correct: true },
-      { text: 'Neither value nor type', correct: false },
-    ]
-  },
-  {
-    question: 'Which method is used to convert a JSON string to a JavaScript object?',
-    answer: [
-      { text: 'JSON.parse()', correct: true },
-      { text: 'JSON.stringify()', correct: false },
-      { text: 'JSON.toObject()', correct: false },
-      { text: 'JSON.convert()', correct: false },
-    ]
-  },
-  {
-    question: 'Which of the following is NOT a JavaScript data type?',
-    answer: [
-      { text: 'Number', correct: false },
-      { text: 'String', correct: false },
-      { text: 'Boolean', correct: false },
-      { text: 'Character', correct: true },
-    ]
-  }
+    {
+        question: 'Which of the following is used to declare a variable in JavaScript?',
+        answer: [
+            { text: 'var', correct: true },
+            { text: 'vbl', correct: false },
+            { text: 'letgo', correct: false },
+            { text: 'variable', correct: false },
+        ]
+    },
+    {
+        question: 'What does `===` operator check in JavaScript?',
+        answer: [
+            { text: 'Only value equality', correct: false },
+            { text: 'Only type equality', correct: false },
+            { text: 'Value and type equality', correct: true },
+            { text: 'Neither value nor type', correct: false },
+        ]
+    },
+    {
+        question: 'Which method is used to convert a JSON string to a JavaScript object?',
+        answer: [
+            { text: 'JSON.parse()', correct: true },
+            { text: 'JSON.stringify()', correct: false },
+            { text: 'JSON.toObject()', correct: false },
+            { text: 'JSON.convert()', correct: false },
+        ]
+    },
+    {
+        question: 'Which of the following is NOT a JavaScript data type?',
+        answer: [
+            { text: 'Number', correct: false },
+            { text: 'String', correct: false },
+            { text: 'Boolean', correct: false },
+            { text: 'Character', correct: true },
+        ]
+    }
 ];
 
 let currentQuestionIndex = 0;
 let score = 0;
 let availableQuestion = [...questions]
 
-function showQst(){
+function showQst() {
     const qst = questions[currentQuestionIndex];
     document.querySelector('.question').textContent = qst.question;
 
     const answerBtn = document.querySelector('.option');
     answerBtn.textContent = " ";
 
-    for(let i = 0; i < qst.answer.length; i++){
+    for (let i = 0; i < qst.answer.length; i++) {
 
+        const btn = document.createElement('button');
+        btn.className = 'option';
+        answerBtn.appendChild(btn);
+        btn.textContent = qst.answer[i].text;
 
-        const btn = document.createElement('button'); 
-        btn.textContent = qst.answer[i].text;        
-        btn.className = 'option';     
-        btn.addEventListener("click", function(){
-             checkAnswer(i);
+        btn.addEventListener("click", function () {
+            checkAnswer(i, btn);
         })
-         answerBtn.appendChild(btn);
     }
+
+    const progress = ((currentQuestionIndex) / questions.length) * 100;
+    const progressBar = document.getElementById('progressBar').style.width = progress + "%";
+
 }
 
-            
+
+function checkAnswer(i, btn) {
+    const qst = questions[currentQuestionIndex];
+
+    if (qst.answer[i].correct) {
+        btn.style.backgroundColor = '#97f173ff'
+        score++;
+    } else {
+        btn.style.backgroundColor = '#d35555ff'
+    }
+
+    currentQuestionIndex++;
+    setTimeout(() => {
+        if (currentQuestionIndex < questions.length) {
+            showQst();
+        } else {
+            alert("Quiz finished! Score: " + score);
+        }
+    }, 600)
+
+}
 showQst();
- 
